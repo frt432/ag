2132f1afb6fcef8d9b6286fba5762d2f869adea0
frt432 <agrod2001@gmail.com>
2026-09-10 01:39:15 +0300

    filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
 
-    // Твои кастомные года
     var customYears = [
         '2027',
         '2026-2027',
@@ -10,34 +9,45 @@
         '2023-2027'
     ];
 
-    // Твои кастомные рейтинги (с шагом 0.5)
     var customRatings = [
-        '5', '5.5', '6', '6.5', 
-        '7', '7.5', '8', '8.5', '9'
+        '5',
+        '5.5',
+        '6',
+        '6.5',
+        '7',
+        '7.5',
+        '8',
+        '8.5',
+        '9'
     ];
 
     Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') {
-            // Подменяем метод рендера фильтров, чтобы внедрить свои значения
-            var originalFilterRender = Lampa.Filter.prototype.render;
-            
-            Lampa.Filter.prototype.render = function () {
-                var renderResult = originalFilterRender.apply(this, arguments);
-                
-                // Находим массивы с годами и рейтингами в текущем инстансе фильтра и заменяем их
-                if (this.arrays) {
-                    if (this.arrays.years) {
-                        this.arrays.years = customYears;
-                    }
-                    if (this.arrays.rating) {
-                        // В Lampa рейтинг обычно передается как vote_average
-                        this.arrays.rating = customRatings; 
-                    }
+        if (e.type !== 'ready') return;
+
+        var originalRender = Lampa.Filter.prototype.render;
+
+        Lampa.Filter.prototype.render = function () {
+
+            // Меняем значения ДО оригинального рендера
+            if (this.arrays) {
+
+                if (Array.isArray(this.arrays.years)) {
+                    this.arrays.years = customYears.slice();
                 }
-                return renderResult;
-            };
-            
-            console.log('Lampa Custom Filters Plugin loaded!');
-        }
+
+                if (Array.isArray(this.arrays.rating)) {
+                    this.arrays.rating = customRatings.slice();
+                }
+
+                // На случай, если в этой версии Lampa рейтинг называется vote_average
+                if (Array.isArray(this.arrays.vote_average)) {
+                    this.arrays.vote_average = customRatings.slice();
+                }
+            }
+
+            return originalRender.apply(this, arguments);
+        };
+
+        console.log('[Custom Filters] loaded');
     });
 })();
